b96dc2e0bf0a6cf1aff2896cb08b5893940fb4e9
APratham <a.pratham1998@gmail.com>
2021-03-22 11:11:59 +0530

    Added all files

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ import base64
 from typing import List
 from github import Github, GithubException
 
+STARTS_WITH = "<!--STARTS_HERE_QUOTE_README-->"
+ENDS_WITH = "<!--ENDS_HERE_QUOTE_README-->"
+REPL_PATTERN = f"{STARTS_WITH}[\\s\\S]+{ENDS_WITH}"
+
 REPOSITORY = os.getenv("INPUT_REPOSITORY")
 GH_TOKEN = os.getenv("INPUT_GH_TOKEN")
 COMMIT_MSG = os.getenv("INPUT_COMMIT_MESSAGE")
@@ -80,6 +84,37 @@ def get_text_to_display() -> str:
     return text_to_display
 
 
+def decode_readme(data: str) -> str:
+    """
+    Decode the contents of old readme.
+    """
+    decoded_bytes = base64.b64decode(data)
+    return str(decoded_bytes, 'utf-8')
+
+
+def generate_new_readme(readme: str, i_tag: str) -> str:
+    """
+    Generate a new Readme.
+    """
+    update_readme_with = f"{STARTS_WITH}\n{i_tag}\n{ENDS_WITH}"
+    return re.sub(REPL_PATTERN, update_readme_with, readme)
+
+
 if __name__ == "__main__":
+    g = Github(GH_TOKEN)
+    try:
+        readme_repo = g.get_repo(REPOSITORY)
+    except GithubException:
+        print("Authentication Error. Try saving a GitHub Token in your Repo Secrets or Use the GitHub Actions Token, which is automatically used by the action.")
+        sys.exit(1)
     text_to_display = get_text_to_display()
-    print('text_to_display')
+    readme_obj = readme_repo.get_readme()
+    readme_content = readme_obj.content
+    readme_content_decoded = decode_readme(readme_content)
+    new_readme = generate_new_readme(readme=readme_content_decoded, i_tag=text_to_display)
+    if readme_content_decoded != new_readme:
+        readme_repo.update_file(path=readme_obj.path, message=COMMIT_MSG,
+                             content=new_readme, sha=readme_obj.sha)
+        print("Success")
+    else:
+        print('No change')
